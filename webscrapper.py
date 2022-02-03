70f2d72a2127510df04add6561de0a2640e835f0
tobes898 <adventureguy898@gmail.com>
2022-02-03 14:42:37 -0500

    Deprecated V1.1 usage

--- webscrapper.py
+++ webscrapper.py
@@ -5,7 +5,6 @@ import os
 import json
 
 from win_notification import WindowAlert
-# import win_notification
 # Variables for Keys and Tokens
 localConsumerKey = None
 localConsumerSecret = None
@@ -44,22 +43,27 @@ def setupTweepyClient():
     setAuthForTweepy()
     return tweepy.Client(localBearerToken,localConsumerKey,localConsumerSecret,localAccessToken,localTokenSecret)
 
-def setupTweepyAPI():
-    setAuthForTweepy()
-    auth = tweepy.OAuthHandler(consumer_key=localConsumerKey, consumer_secret=localConsumerSecret)
-    auth.set_access_token(localAccessToken, localTokenSecret)
-    return tweepy.API(auth)
+# Deprecating old V1.1 code method
+# def setupTweepyAPI():
+#     setAuthForTweepy()
+#     auth = tweepy.OAuthHandler(consumer_key=localConsumerKey, consumer_secret=localConsumerSecret)
+#     auth.set_access_token(localAccessToken, localTokenSecret)
+#     return tweepy.API(auth)
 
 def monitorTwitterAccount(screen_name):
-    api = setupTweepyAPI()
-# pip install win10toast
     targeted_screen_name = screen_name
-    user = api.get_user(screen_name = targeted_screen_name)
-    print(user.id)
+
+    # Old V1.1 Code
+    # api = setupTweepyAPI()
+    # user = api.get_user(screen_name = targeted_screen_name)
+   
+
     client = setupTweepyClient()
+    user = client.get_user(username=screen_name)
     printer = TweetPrinter(localConsumerKey, localConsumerSecret, localAccessToken, localTokenSecret)
     printer.setTargetAccount(targeted_screen_name)
-    printer.filter(follow = [user.id], filter_level='low')
+    print(user.data['id'])
+    printer.filter(follow = [user.data['id']], filter_level='low')
 
 class TweetPrinter(tweepy.Stream):
     targetAccount = ''
@@ -75,7 +79,6 @@ class TweetPrinter(tweepy.Stream):
 
     def on_data(self, status):
         data = json.loads(status)
-        
         user = data['user']['screen_name']
         tweet_id = data['id']
         tweet = twitterClient.get_tweet(id=tweet_id)
@@ -83,3 +86,6 @@ class TweetPrinter(tweepy.Stream):
         if(user==targetAccount):
             notifyClass.MyNotification(str(tweet[0]))
             print(tweet[0])
+
+
+
